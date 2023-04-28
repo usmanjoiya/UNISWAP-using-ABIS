5f83f0393bded94a7a0cb7c7a176b9c52bc083c3
Usman Akbar <45451919+usmanjoiya@users.noreply.github.com>
2023-04-28 17:42:38 +0500

    Update abis.js

--- packages/contracts/src/abis.js
+++ packages/contracts/src/abis.js
@@ -5,9 +5,10 @@ import router01 from "./abis/router01.json";
 import router02 from "./abis/router02.json";
 
 export default {
-  erc20,
-  factory,
+
   pair,
   router01,
   router02,
+  erc20,
+  factory,
 };
